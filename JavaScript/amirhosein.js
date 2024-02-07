bc1baf657409bcaea3cb856da01423aeae53b173
dazrwf <anonymoous.xo@gmail.com>
2024-02-07 09:30:50 -0800

    1st commit

---
+++ JavaScript/amirhosein.js
@@ -0,0 +1,17 @@
+let firstNmae = "Amirhosein"
+let lastName = "Rezaie"
+let fos = "Network & SoftWare"
+let gpa = "18"
+let age = 18
+let shamsi = 1402 - age
+let miladi = 2024 - age
+
+document.write(`<div class="card">
+<p>First Name : ${firstNmae}</p>
+<p>Last Name : ${lastName}</p>
+<p>Field of Study : ${fos}</p>
+<p>GPA : ${gpa}</p>
+<p>Age : ${age}</p>
+<p>Born in Shamsi : ${shamsi}</p>
+<p>born in Miladi : ${miladi}</p>
+</div>`)
---
+++ JavaScript/nima.js
@@ -0,0 +1,17 @@
+let firstNmae = "Nima"
+let lastName = "Riyahi"
+let fos = "Network & SoftWare"
+let gpa = "0"
+let age = 17
+let shamsi = 1402 - age
+let miladi = 2024 - age
+
+document.write(`<div class="card">
+<p>First Name : ${firstNmae}</p>
+<p>Last Name : ${lastName}</p>
+<p>Field of Study : ${fos}</p>
+<p>GPA : ${gpa}</p>
+<p>Age : ${age}</p>
+<p>Born in Shamsi : ${shamsi}</p>
+<p>born in Miladi : ${miladi}</p>
+</div>`)
